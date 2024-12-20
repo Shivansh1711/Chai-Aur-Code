53dfaed2832b92c56ac5df969256879354588c94
Shivansh <ojharishu000@gmail.com>
2024-12-20 15:38:25 +0530

    different datatypes

---
+++ data_types.js
@@ -0,0 +1,10 @@
+const regNo = 12216507
+let name = "Shivansh"
+let age = 20
+let weight = 57.5
+let city = "Ara"
+let is_legal = true
+let card = null
+let bill;
+
+console.table([regNo,name,age,weight,city,is_legal,card,bill])
